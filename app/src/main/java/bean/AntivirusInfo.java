696a88d60973979a7b51a5f8ce90cb7585ee4e34
developerzhangjie@gmail.com <zj20163.1415926>
2016-11-22 21:22:00 +0800

    2016 11 22

---
+++ app/src/main/java/bean/AntivirusInfo.java
@@ -0,0 +1,15 @@
+package bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Sin on 2016/10/15.
+ * Description:病毒的信息描述
+ */
+
+public class AntivirusInfo {
+    public String packageName;
+    public String name;
+    public Drawable icon;
+    public boolean isAntivirus;
+}
